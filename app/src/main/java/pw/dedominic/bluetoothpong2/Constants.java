950a441df9dbb425b12373d18fd7a9c9684d4bd5
SprattStud <sprattmanx@yahoo.com>
2015-04-19 08:07:52 -0400

    Updated Constants.java
    alterations were primarily a shift from ball->shell and paddle ->artillery

--- app/src/main/java/pw/dedominic/bluetoothpong2/Constants.java
+++ app/src/main/java/pw/dedominic/bluetoothpong2/Constants.java
@@ -1,7 +1,8 @@
-package pw.dedominic.bluetoothpong2;
+package main.java.pw.dedominic.bluetoothpong2;
 
 /**
  * Created by qemu on 2/22/2015.
+ * Edited by Spratt on 4/19/2015
  */
 public interface Constants
 {
@@ -19,28 +20,29 @@ public interface Constants
 	public final int DISCONNECTED = 0;
 	public final int CONNECTED = 1;
 	public final int READY = 'r';
-	public final int BALLX_READ = 'x';
-	public final int BALLY_READ = 'y';
-	public final int BALL_ANG_READ = 'a';
-	public final int PADDLE_READ = 'p';
+	public final int SHELLX_READ = 'x';
+	public final int SHELLY_READ = 'y';
+	public final int SHELL_ANG_READ = 'a';
+	public final int ARTILLERY_ANG_READ = 'p';
 	public final int GAME_OVER_READ = 'g';
 
 	// handler messages - writing
 	public final int GAME_OVER = -1;
 	public final int SEND_GAME_STATE = 0;
-	public final int SEND_PADDLE = 1;
-	public final int SEND_BALL_ANGLE = 2;
+	public final int SEND_ARTILLERY_ANGLE = 1;
+	public final int SEND_SHELL_ANGLE = 2;
 
-	// GAME LAYOUT CONSTANTS
-	public static final float PADDLE_SPACE_FRACT = 100;
-	public static final float PADDLE_HALFWIDTH_FRACT = 100;
-	public static final float PADDLE_HALFHEIGHT_FRACT = 8;
-	public static final float BALL_RADIUS_FRACT = 100;
+	// GAME LAYOUT CONSTANTS - CHANGED VALUES AS THE ARTILLERY JUST SHIFT VIA A ROOT
+	// THEY DON'T REALLY NEED TO MOVE AT ALL FOR CURRENT VERSION
+	public static final float ARTILLERY_SPACE_FRACT = 100;
+	public static final float ARTILLERY_WIDTH_FRACT = 200;
+	public static final float ARTILLERY_HEIGHT_FRACT = 16;
+	public static final float SHELL_RADIUS_FRACT = 100;
 	public static final int PAINT_COLOR = 0xff666666; // grey
 
 	// GAME FRAMES PER SECOND
 	public static final int FPS = 60;
 
 	// GAME OTHER
-	public static final int PADDLE_MOVE_ACCEL = 2;
+	public static final int SHELL_ROTATE_ACCEL = 2;
 }
